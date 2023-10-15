ce56198c9836b7fc8bb8c78dd1990d1d18770fce
angersprite <52611253+angersprite@users.noreply.github.com>
2023-10-15 11:53:06 -0500

    separated await for class selection, fixes issue with random class not getting weapon/armor

--- npcGenerator.js
+++ npcGenerator.js
@@ -4,26 +4,17 @@ import NPC from './NPC.js'
 export default async function generateNPC(classID, raceID) {
     let npc = new NPC
 
+    await goonrDAO.getClass(classID)
+    .then(c => {
+        npc.className = c[0].name
+        npc.hitPoints = Math.floor(Math.random() * (c[0].hit_die - 1 + 1)) + 1
+        npc.toHit = c[0].thac0
+        classID = c[0].id
+    })
     await Promise.all([
-        goonrDAO.getClass(classID)
-            .then(c => {
-                npc.className = c[0].name
-                npc.hitPoints = Math.floor(Math.random() * (c[0].hit_die - 1 + 1)) + 1
-                npc.toHit = c[0].thac0
-                classID = c[0].id
-            }),
-        goonrDAO.getRace(raceID)
-            .then(r => {
-                npc.race = r[0].name
-            }),
-        goonrDAO.getFirstName()
-            .then(fn => {
-                npc.firstName = fn
-            }),
-        goonrDAO.getLastName()
-            .then(ln => {
-                npc.lastName = ln
-            }),
+        goonrDAO.getRace(raceID) .then(r => { npc.race = r[0].name }),
+        goonrDAO.getFirstName() .then(fn => { npc.firstName = fn  }),
+        goonrDAO.getLastName().then(ln => { npc.lastName = ln  }),
         goonrDAO.getWeapon(classID)
             .then(w => {
                 if (w.length > 0) {
@@ -38,19 +29,10 @@ export default async function generateNPC(classID, raceID) {
                     npc.armorClass = a[0].armor_class
                 }
             }),
-        goonrDAO.getDescriptors(1)
-            .then(desc => {
-                npc.descriptors = desc
-            }),
-        goonrDAO.getSkills(classID, 1)
-            .then(skills => {
-                npc.skills = skills
-            }),
-        goonrDAO.getItems(1)
-            .then(i => {
-                npc.items = i
-            })
+        goonrDAO.getDescriptors(1) .then(desc => { npc.descriptors = desc }),
+        goonrDAO.getSkills(classID, 1).then(skills => { npc.skills = skills }),
+        goonrDAO.getItems(1).then(i => { npc.items = i  })
     ])
-    
+
     return npc
 }
